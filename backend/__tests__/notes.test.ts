8a9191efb1eae61161ed0b262fd09608990ce3e1
Kalpas Wang <wf6212@gmail.com>
2023-07-27 15:15:39 +0800

    first commit

---
+++ backend/__tests__/notes.test.ts
@@ -0,0 +1,7 @@
+describe('notes', () => {
+  describe('GET /api/notes', () => {
+    it('should return 200 OK', () => {
+      expect(true).toBe(true);
+    });
+  });
+});
---
+++ backend/src/server.ts
@@ -0,0 +1,2 @@
+import app from './app';
+const port = 3838;
